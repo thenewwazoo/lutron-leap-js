ddf3fd3cf455ecc24c70c4d1cc42e25b1999cb2a
Justin Wood <jwood@me.com>
2022-01-30 20:49:06 -0800

    Initial checkin

--- src/MessageBodyTypes.ts
+++ src/MessageBodyTypes.ts
@@ -10,11 +10,22 @@ export class UnimplementedMessageBodyType {
 }
 
 export type MessageBodyType =
+    | 'OneProjectDefinition'
+    | 'OnePresetDefinition'
+    | 'OneAreaSceneDefinition'
+    | 'OneLinkNodeDefinition'
+    | 'MultipleLinkNodeDefinition'
+    | 'OneControlStationDefinition'
+    | 'OneAreaDefinition'
+    | 'MultipleAreaDefinition'
+    | 'OneAreaStatus'
+    | 'MultipleAreaStatus'
     | 'OneDeviceStatus'
     | 'OneDeviceDefinition'
     | 'MultipleDeviceDefinition'
     | 'OneZoneDefinition'
     | 'OneZoneStatus'
+    | 'MultipleZoneStatus'
     | 'OnePingResponse'
     | 'OneButtonGroupDefinition'
     | 'OneButtonDefinition'
@@ -25,6 +36,21 @@ export class OneDeviceStatus {
     DeviceStatus!: DeviceStatus;
 }
 
+export class OneAreaSceneDefinition {
+    AreaSceneDefinition!: AreaSceneDefinition;
+}
+
+export class OnePresetDefinition {
+    PresetDefinition!: PresetDefinition;
+}
+
+export class OneLinkNodeDefinition {
+    LinkNodeDefinition!: LinkNodeDefinition;
+}
+
+export class MultipleLinkNodeDefinition {
+    Links!: LinkNodeDefinition[];
+}
 export class OneDeviceDefinition {
     Device!: Device;
 }
@@ -33,14 +59,40 @@ export class MultipleDeviceDefinition {
     Devices: Device[] = [];
 }
 
+export class MultipleAreaDefinition {
+    Areas: AreaDefinition[] = [];
+}
+
 export class OneZoneDefinition {
     ZoneDefinition!: ZoneDefinition;
+}
+
+export class OneProjectDefinition {
+    ProjectDefinition!: ProjectDefinition;
+}
+
+export class OneAreaStatus {
+    AreaStatus!: AreaStatus;
+}
+
+export class MultipleAreaStatus {
+    AreaStatuses!: AreaStatus[]
+}
+export class OneAreaDefinition {
+    AreaDefinition!: AreaDefinition;
+}
+
+export class OneControlStationDefinition {
+    ControlStationDefinition!: ControlStationDefinition;
 }
 
 export class OneZoneStatus {
     ZoneStatus!: ZoneStatus;
 }
 
+export class MultipleZoneStatus {
+    ZonsStatuses!: ZoneStatus[]
+}
 export class OnePingResponse {
     PingResponse!: {
         LEAPVersion: number;
@@ -64,10 +116,22 @@ export class ExceptionDetail {
 }
 
 export type BodyType =
+    | OneProjectDefinition
+    | OnePresetDefinition
+    | OneAreaSceneDefinition
+    | OneLinkNodeDefinition
+    | MultipleLinkNodeDefinition
+    | OneZoneDefinition
+    | OneAreaDefinition
+    | MultipleAreaDefinition
+    | OneControlStationDefinition
+    | OneAreaStatus
+    | MultipleAreaStatus
     | OneDeviceStatus
     | OneDeviceDefinition
     | MultipleDeviceDefinition
     | OneZoneStatus
+    | MultipleZoneStatus
     | OnePingResponse
     | OneButtonGroupDefinition
     | OneButtonDefinition
@@ -81,6 +145,15 @@ export function parseBody(type: MessageBodyType, data: object): BodyType {
         case 'OneDeviceDefinition':
             theType = OneDeviceDefinition;
             break;
+        case 'OnePresetDefinition':
+            theType = OnePresetDefinition;
+            break;
+        case 'OneAreaSceneDefinition':
+            theType = OneAreaSceneDefinition;
+            break;
+        case 'MultipleAreaDefinition':
+            theType = MultipleAreaDefinition;
+            break;
         case 'MultipleDeviceDefinition':
             theType = MultipleDeviceDefinition;
             break;
@@ -89,6 +162,9 @@ export function parseBody(type: MessageBodyType, data: object): BodyType {
             break;
         case 'OneZoneStatus':
             theType = OneZoneStatus;
+            break;
+        case 'MultipleZoneStatus':
+            theType = MultipleZoneStatus;
             break;
         case 'OnePingResponse':
             theType = OnePingResponse;
@@ -104,6 +180,30 @@ export function parseBody(type: MessageBodyType, data: object): BodyType {
             break;
         case 'OneDeviceStatus':
             theType = OneDeviceStatus;
+            break;
+        case 'OneZoneDefinition':
+            theType = OneZoneDefinition;
+            break;
+        case 'OneAreaDefinition':
+            theType = OneAreaDefinition;
+            break;
+        case 'OneAreaStatus':
+            theType = OneAreaStatus;
+            break;
+        case 'MultipleAreaStatus':
+            theType = MultipleAreaStatus;
+            break;
+        case 'OneControlStationDefinition':
+            theType = OneControlStationDefinition;
+            break;
+        case 'OneProjectDefinition':
+            theType = OneProjectDefinition;
+            break;
+        case 'OneLinkNodeDefinition':
+            theType = OneLinkNodeDefinition;
+            break;
+        case 'MultipleLinkNodeDefinition':
+            theType = MultipleLinkNodeDefinition;
             break;
         default:
             throw new UnimplementedMessageBodyType(type as string);
@@ -252,3 +352,63 @@ export type DeviceHeard = {
     // UnassociatedDeviceDiscoverSession: Href;
     // PairedDevices: PairedDevice[]
 };
+
+type AreaStatus = Href & {
+    Level: number;
+    OccupancyStatus: string;
+    CurrentScene: Href;
+};
+
+type AreaDefinition = Href & {
+    Name: string;
+    ControlType: string;
+    Parent: Href;
+    AssociatedZones: Href[];
+    AssociatedControlStations: Href[];
+};
+
+type ControlStationDefinition = Href & {
+    Name: string;
+    ControlType: string;
+    Parent: Href;
+    AssociatedArea: Href;
+    SortOrder: number
+    AssociatedGangedDevices: Device[];
+};
+
+type ProjectDefinition = Href & {
+    Name: string;
+    ControlType: string;
+    ProductType: string;
+    Contacts: Href[];
+    TimeclockEventRules: Href;
+    ProjectModifiedTimestamp: {
+        Year: number;
+        Month: number;
+        Day: number;
+        Hour: number;
+        Minute: number;
+        Second: number;
+        Utc: 'string';
+    };
+};
+
+type LinkNodeDefinition = Href & {
+    Parent: Href;
+    LinkType: string;
+    SortOrder: number;
+    // RFProperties: {json}
+    AssociatedLink: Href;
+};
+
+type AreaSceneDefinition = Href & {
+    Name: string;
+    Parent: Href;
+    Preset: Href;
+    SortOrder: number;
+};
+
+type PresetDefinition = Href & {
+    Parent: Href;
+};
+
--- src/Messages.test.ts
+++ src/Messages.test.ts
@@ -126,3 +126,91 @@ test('mangled', () => {
     expect(resp.CommuniqueType).toBeUndefined();
     expect(resp.Header.StatusCode).toBeUndefined();
 });
+
+test('OneAreaStatus', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneAreaStatus","StatusCode":"200 OK","Url":"/area/729/status","ClientTag":"afbacb1f-32bd-47de-9a2a-191e60e5c2a9"},"Body":{"AreaStatus":{"href":"/area/729/status","Level":0,"OccupancyStatus":"Unoccupied","CurrentScene":{"href":"/areascene/733"}}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.AreaStatus.OccupancyStatus).toEqual('Unoccupied');
+});
+
+test('OneZoneDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneZoneDefinition","StatusCode":"200 OK","Url":"/zone/622","ClientTag":"ec1ec311-1e53-439d-ac46-0908252d7d08"},"Body":{"Zone":{"href":"/zone/622","Name":"Loft Outdoors","ControlType":"Switched","Category":{"Type":"OtherAmbient","IsLight":true},"AssociatedArea":{"href":"/area/729"},"SortOrder":0}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.Zone.ControlType).toEqual('Switched');
+});
+
+test('OneAreaDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneAreaDefinition","StatusCode":"200 OK","Url":"/area/729","ClientTag":"2ac306b1-13f3-4d11-a4d7-b5b4776311f3"},"Body":{"Area":{"href":"/area/729","Name":"Loft","Parent":{"href":"/area/24"},"AssociatedZones":[{"href":"/zone/622"}],"AssociatedControlStations":[{"href":"/controlstation/613"},{"href":"/controlstation/750"}]}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.Area.Name).toEqual('Loft');
+});
+
+test('OneControlStationDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneControlStationDefinition","StatusCode":"200 OK","Url":"/controlstation/613","ClientTag":"0020d264-db53-47d7-b3fe-1b07017c3055"},"Body":{"ControlStation":{"href":"/controlstation/613","Name":"Loft Outdoor Lights","AssociatedArea":{"href":"/area/729"},"SortOrder":0,"AssociatedGangedDevices":[{"Device":{"href":"/device/615","DeviceType":"SunnataSwitch","AddressedState":"Addressed"},"GangPosition":0}]}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.ControlStation.Name).toEqual('Loft Outdoor Lights');
+});
+
+test('OneProjectDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneProjectDefinition","StatusCode":"200 OK","Url":"/project","ClientTag":"8c5c9547-f246-4991-8150-6d84f76e9046"},"Body":{"Project":{"href":"/project","Name":"Home","ProductType":"Lutron RadioRA 3 Project","MasterDeviceList":{"Devices":[{"href":"/device/96"}]},"Contacts":[{"href":"/contactinfo/81"}],"TimeclockEventRules":{"href":"/project/timeclockeventrules"},"ProjectModifiedTimestamp":{"Year":2022,"Month":1,"Day":28,"Hour":19,"Minute":24,"Second":29,"Utc":"0"}}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.Project.Name).toEqual('Home');
+});
+
+test('OneLinkNodeDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneLinkNodeDefinition","StatusCode":"200 OK","Url":"/device/615/linknode/617","ClientTag":"3a784f3d-660b-4eac-8dd8-b34127e1e1fe"},"Body":{"LinkNode":{"href":"/device/615/linknode/617","Parent":{"href":"/device/615"},"LinkType":"ClearConnectTypeX","RFProperties":{},"AssociatedLink":{"href":"/link/98"}}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.LinkNode.LinkType).toEqual('ClearConnectTypeX');
+});
+
+test('OneAreaSceneDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OneAreaSceneDefinition","StatusCode":"200 OK","Url":"/areascene/734","ClientTag":"5f7ed150-b163-4535-9cd1-0a641fa651a4"},"Body":{"AreaScene":{"href":"/areascene/734","Name":"Scene 001","Parent":{"href":"/area/729"},"Preset":{"href":"/preset/734"},"SortOrder":1}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.AreaScene.Name).toEqual('Scene 001');
+});
+
+test('OnePresetDefinition', () => {
+    const line =
+        '{"CommuniqueType":"ReadResponse","Header":{"MessageBodyType":"OnePresetDefinition","StatusCode":"200 OK","Url":"/preset/734","ClientTag":"1054a432-f650-4985-bdd2-f2e9ebaa7278"},"Body":{"Preset":{"href":"/preset/734","Parent":{"href":"/areascene/734"}}}}';
+
+    const response: Response = Response.fromJSON(JSON.parse(line));
+    expect(response?.Header.StatusCode?.code).toEqual(200);
+    expect(response?.CommuniqueType).toEqual('ReadResponse');
+    // @ts-ignore
+    expect(response.Body.Preset.Parent).toEqual({"href":"/areascene/734"});
+});
